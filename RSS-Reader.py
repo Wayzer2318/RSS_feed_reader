7d825ec835515d5a7829f8057a11816b49d931f5
wayzer2318 <arezki.ait.kettout@gmail.com>
2025-12-02 16:52:24 +0100

    updated version of the RSS fead reader
    added the ability to give more then one url

--- RSS-Reader.py
+++ RSS-Reader.py
@@ -2,20 +2,38 @@ import feedparser
 
 
 def read_rss_feed(feed_url):
-    # parse the url
-    myfeed = feedparser.parse(feed_url)
+    for url in feed_url:
+        print(f"\n{'=' * 50}\nFetching: {url}\n{'=' * 50}\n")
+        # parse the url
+        myfeed = feedparser.parse(url)
 
-    # print metadata
-    print(f"feed title: {myfeed.feed.title}")
-    print(f"feed link : {myfeed.feed.link}\n")
+        # print metadata
+        print(f"feed title: {myfeed.feed.title}")
+        print(f"feed link : {myfeed.feed.link}\n")
 
-    # print entries
-    for entry in myfeed.entries[:5]:
-        print(f"title : {entry.title}")
-        print(f"link : {entry.link}")
-        print(f" description : {entry.description}\n")
+        # print entries
+        for entry in myfeed.entries[:5]:
+            print(f"title : {entry.title}")
+            print(f"link : {entry.link}")
+            print(f" description : {entry.description}\n")
 
 
-feed_url = input("please enter your feed url : ")
+def get_user_feed():
+    feeds = []
+    while True:
+        url = input("> ").strip()
+        if not url:
+            break
+        feeds.append(url)
+    return feeds
 
-read_rss_feed(feed_url)
+
+print(" plz enter ur urls and if press 2 times enter")
+# get user feeds
+feed_url = get_user_feed()
+
+
+if feed_url:
+    read_rss_feed(feed_url)
+else:
+    print(" no RSS feed url provided.")
